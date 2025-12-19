207f2935e703e9603d130058ebc42892b740cd8e
farizakb <farizakbarzada@gmail.com>
2025-12-19 20:15:04 +0400

    commit 9

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,6 +2,15 @@
 """ Lists all documents in a collection """
 
 def list_all(mongo_collection):
-    """Lists all documents in a collection"""
-    documents = list(mongo_collection.find())
-    return documents
+    """
+    Lists all documents in a MongoDB collection.
+
+    Args:
+        mongo_collection: pymongo collection object
+
+    Returns:
+        List of documents, or empty list if none found
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
