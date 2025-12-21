da4abd8ac819f8cbd3427a67a5f3241a9c90db90
farizakb <farizakbarzada@gmail.com>
2025-12-21 08:34:40 -0600

    commit 12.1212

--- NoSQL/102-log_stats.py
+++ NoSQL/102-log_stats.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Module for logging stats"""
+"""Module for improved version of log_stats function"""
 from pymongo import MongoClient
 
 
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+PIPE = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}, {"$limit": 10}]
 
 
 def log_stats(mongo_collection, option=None):
-    """Function to get nginx stats from mongodb"""
+    """Function for logging stats"""
     items = {}
     if option:
         value = mongo_collection.count_documents(
@@ -22,6 +24,10 @@ def log_stats(mongo_collection, option=None):
         log_stats(nginx_collection, method)
     status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
+    print("IPs:")
+
+    for ip in mongo_collection.aggregate(PIPE):
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
 
 
 if __name__ == "__main__":
